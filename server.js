cd0a0952ab97af568dfbbf1c1a6483464d6a6e05
AustinOsti <austin.nyika2015@gmail.com>
2019-03-03 21:28:12 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const axios = require('axios');
+const request = require('request');
 const cheerio = require('cheerio');
+
+const fs = require('fs');
+const file = fs.createWriteStream('./tips.txt');
+
+/* send console.log to tips.txt file ...
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/tips.txt', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+	log_file.write(util.format(d) + '\n');
+	log_stdout.write(util.format(d) + '\n');
+};
+*/
 
 const url = 'https://www.bettingtips1x2.com/';
 
@@ -20,18 +36,20 @@ let getData = function(html){
 	const $ = cheerio.load(html);	
 	$('table.results tr')
 	.each(function(i, elem){
-		var game = $(elem).find('a').text();
+		var game = $(elem).children("td:nth-child(4)").text();
 		if (game.length > 0) {
 			data.push({
 				date: d,
 				league: $(elem).children("td:nth-child(2)").text(),
-				game: game,
+				game: game.replace('-', 'vs'),
 				odds_1:	$(elem).children("td:nth-child(5)").text(),
 				odds_X: $(elem).children("td:nth-child(6)").text(),
 				odds_2: $(elem).children("td:nth-child(7)").text(),
-				tip: $(elem).children("td:nth-child(8)").text()
+				tip: $(elem).children("td:nth-child(8)").text().replace(':', '--')
 			});			
 		}	
 	});
-	console.log(data);	
+	file.write(JSON.stringify(data));
+	file.end();	
+//	console.log(data);	
 }
